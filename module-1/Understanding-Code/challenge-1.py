445e0905e01a71856e7d08e5a51b366c0a30beee
Fergus <ferguscuthill@googlemail.com>
2020-05-25 17:36:57 +0100

    Added solutions for understanding others code and OOP labs

---
+++ module-1/Understanding-Code/challenge-1.py
@@ -0,0 +1,39 @@
+"""
+This is a dumb calculator that can add and subtract whole numbers from zero to five.
+When you run the code, you are prompted to enter two numbers (in the form of English
+word instead of number) and the operator sign (also in the form of English word).
+The code will perform the calculation and give the result if your input is what it
+expects.
+
+"""
+
+print('Welcome to this calculator!')
+print('It can add and subtract whole numbers from zero to five')
+a = input('Please choose your first number (zero to five): ')
+b = input('What do you want to do? plus or minus: ')
+c = input('Please choose your second number (zero to five): ')
+
+dict_num = {'zero':0,'one':1, 'two':2,'three':3,'four':4, 'five':5}
+
+if not a in dict_num:
+    print("I am not able to answer this question. Check your input.")
+elif not c in dict_num:
+    print("I am not able to answer this question. Check your input.")
+# FC comment - this should be an "and", not an "or"
+elif b != 'plus' or b != 'minus':
+    print("I am not able to answer this question. Check your input.")
+else:
+    # FC comment - need to extend this list to ten. Also need to deal with negative numbers
+    number_to_text = ['zero', 'one', 'two', 'three', 'four', 'five']
+
+    num_a = dict_num [a]
+    num_c = dict_num [c]
+
+    # FC comment - don't need to define result here 
+    result = 0
+    if b == 'plus':
+        result = num_a + num_c
+    else:
+        result = num_a - num_c
+    
+    print(a, b, c, 'equals', number_to_text[result])
